f411080acf84e0e966ca8ca86e366f44dd7f61e8
trigaten <sanderman5@hotmail.com>
2021-05-04 23:26:28 -0400

    oops

--- inst.sql
+++ inst.sql
@@ -1,67 +1,68 @@
--- What was the most reviewed book in the last 5 years?
-select books.title, text_reviews from ratings
-JOIN books ON books.isbn = ratings.isbn
-WHERE books.publication_date > '2015-05-03'
-ORDER BY ratings.text_reviews DESC
-LIMIT 1
--- Who is the publisher with the most reviewed books (quantity of text reviews) published in the last 10 years?
-SELECT books.publisher, sum(text_reviews) as text_reviews FROM ratings
-JOIN books ON books.isbn = ratings.isbn
-WHERE books.publication_date > '2010-05-03'
-GROUP BY books.publisher
-ORDER BY text_reviews DESC
-LIMIT 1
--- Which are better rated: novellas (<= 200 pages) or other books?
--- Reference: https://bookriot.com/how-long-is-a-short-story/
--- We can see that the first entry, which represents the average rating of novellas if slightly
--- higher than the average rating of non-novellas
-SELECT sum(avg_rating)/count(avg_rating)::float as ratings FROM ratings
-JOIN books ON books.isbn = ratings.isbn
-GROUP BY books.pages <= 200
--- How many books written by J.K. Rowling are in the database?
-SELECT COUNT(*) FROM books
-WHERE author like '%J.K. Rowling%'
+-- Database: books_database
+-- 	don't forget to change the file path for the data import when executing these instructions
+CREATE TABLE raw_books (
+    book_id varchar,
+	title varchar,
+	author varchar,
+	avg_rating numeric,
+	isbn varchar,
+	isbn13 varchar,
+	lang varchar,
+	pages numeric,
+	ratings_count numeric,
+	text_reviews numeric,
+	publication_date date,
+	publisher varchar
+);
 
--- Which books have more than average lifetime revenue?
-SELECT books.title, lifetime_sales.revenue
-FROM
-	books
-JOIN lifetime_sales 
-ON lifetime_sales.isbn = books.isbn
-WHERE
-	lifetime_sales.revenue > (
-		SELECT
-			AVG (revenue)
-		FROM
-			lifetime_sales
-	);
+COPY raw_books
+  FROM '/private/tmp/books-3.csv'
+  WITH (FORMAT CSV, HEADER, DELIMITER ',');
+  
+CREATE TABLE books (
+	isbn varchar,
+	title varchar,
+	author varchar,
+	lang varchar,
+	pages numeric,
+	publication_date date,
+	publisher varchar,
+	PRIMARY KEY(isbn)
+);
 
+CREATE TABLE ratings (
+	isbn varchar,
+	avg_rating numeric,
+	ratings_count numeric,
+	text_reviews numeric,
+	PRIMARY KEY(isbn),
+   	CONSTRAINT for_key
+      FOREIGN KEY(isbn) 
+	  REFERENCES books(isbn)
+);
 
--- What is the best reviewed book on record? We can see that there are multiple.
-SELECT books.title, ratings.avg_rating FROM ratings
-JOIN books ON books.isbn = ratings.isbn
-ORDER BY ratings.avg_rating DESC
+INSERT INTO books (isbn, title, author, lang, pages, publication_date, publisher)
+SELECT isbn, title, author, lang, pages, publication_date, publisher
+FROM raw_books;
 
+INSERT INTO ratings (isbn, avg_rating, ratings_count, text_reviews)
+SELECT isbn, avg_rating, ratings_count, text_reviews
+FROM raw_books;
 
--- How many books are written in English without regional specification?
-SELECT count(books.title) from books
-WHERE books.lang like '%eng%'
--- How many books are written in English including regional specifications?
-SELECT count(books.title) from books
-WHERE books.lang like '%en%'
--- What is the average number of pages of books in the database?
-SELECT avg(books.pages) FROM books
--- What is the average number of pages of highly rated (4.0+) books in the database?
-SELECT avg(books.pages) FROM books 
-JOIN ratings ON books.isbn = ratings.isbn
-WHERE avg_rating >= 4
+CREATE TABLE lifetime_sales (
+	isbn varchar,
+	revenue float,
+	copies_sold integer,
+	PRIMARY KEY(isbn),
+   	CONSTRAINT for_key
+      FOREIGN KEY(isbn) 
+	  REFERENCES books(isbn)
+);
 
--- Which author has the best rated books?
--- We can see that there are a few.
-SELECT books.author, avg(ratings.avg_rating):: NUMERIC(3, 2) FROM ratings 
-JOIN books ON books.isbn = ratings.isbn
-GROUP BY books.author
-ORDER BY avg(ratings.avg_rating) DESC
--- What is the average page length of Harry Potter books in the database
-SELECT avg(pages):: NUMERIC(5, 2) FROM books
-WHERE title LIKE '%Harry Potter%'
+INSERT INTO lifetime_sales (isbn, revenue)
+SELECT isbn, (SELECT random_between(1,100))
+FROM books;
+
+update lifetime_sales
+    set revenue = POWER(10, floor(random() * 4 + 3))*random(),
+	copies_sold = POWER(10, floor(random() * 4 + 3))*random();
--- queries.sql
+++ queries.sql
@@ -1,14 +1,9 @@
-
 -- What was the most reviewed book in the last 5 years?
 select books.title, text_reviews from ratings
 JOIN books ON books.isbn = ratings.isbn
 WHERE books.publication_date > '2015-05-03'
 ORDER BY ratings.text_reviews DESC
 LIMIT 1
--- What is the best reviewed book on record? We can see that there are multiple.
-SELECT books.title, ratings.avg_rating FROM ratings
-JOIN books ON books.isbn = ratings.isbn
-ORDER BY ratings.avg_rating DESC
 -- Who is the publisher with the most reviewed books (quantity of text reviews) published in the last 10 years?
 SELECT books.publisher, sum(text_reviews) as text_reviews FROM ratings
 JOIN books ON books.isbn = ratings.isbn
@@ -23,6 +18,31 @@ LIMIT 1
 SELECT sum(avg_rating)/count(avg_rating)::float as ratings FROM ratings
 JOIN books ON books.isbn = ratings.isbn
 GROUP BY books.pages <= 200
+-- How many books written by J.K. Rowling are in the database?
+SELECT COUNT(*) FROM books
+WHERE author like '%J.K. Rowling%'
+
+-- Which books have more than average lifetime revenue?
+SELECT books.title, lifetime_sales.revenue
+FROM
+	books
+JOIN lifetime_sales 
+ON lifetime_sales.isbn = books.isbn
+WHERE
+	lifetime_sales.revenue > (
+		SELECT
+			AVG (revenue)
+		FROM
+			lifetime_sales
+	);
+
+
+-- What is the best reviewed book on record? We can see that there are multiple.
+SELECT books.title, ratings.avg_rating FROM ratings
+JOIN books ON books.isbn = ratings.isbn
+ORDER BY ratings.avg_rating DESC
+
+
 -- How many books are written in English without regional specification?
 SELECT count(books.title) from books
 WHERE books.lang like '%eng%'
@@ -35,9 +55,7 @@ SELECT avg(books.pages) FROM books
 SELECT avg(books.pages) FROM books 
 JOIN ratings ON books.isbn = ratings.isbn
 WHERE avg_rating >= 4
--- How many books written by J.K. Rowling are in the database?
-SELECT COUNT(*) FROM books
-WHERE author like '%J.K. Rowling%'
+
 -- Which author has the best rated books?
 -- We can see that there are a few.
 SELECT books.author, avg(ratings.avg_rating):: NUMERIC(3, 2) FROM ratings 
